462d536755d8adc040f41a11e0da9489658e0bf0
Chase <chaserabenn@gmail.com>
2020-04-23 02:45:18 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,14 @@ def turn_count(board)
   turn
 end
 
+# def current_player(board)
+#   if turn_count(board) % 2 == 0
+#     return "X"
+#   else
+#     return "O"
+#   end
+# end
+
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
