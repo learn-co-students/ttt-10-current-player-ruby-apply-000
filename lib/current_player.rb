5654978991b6636be975c4192811645b97b20032
bright-suite-6513 <huang.stan@icloud.com>
2020-04-29 03:46:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  index = 0
+
+  board.each do |spot|
+    if spot == "X" || spot == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(some)
+  if turn_count(some).even?
+    return "X"
+  elsif turn_count(some).odd?
+    return "O"
+  end
+end
