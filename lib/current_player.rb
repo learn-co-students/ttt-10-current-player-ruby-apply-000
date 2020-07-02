d1e2a81ff6bbf3fd968f137cb529e59097e5229c
Alice Perlov <aperlov23@gmail.com>
2020-07-02 18:28:06 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,12 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(turn)
-  #counter = 1
   move_count = []
   turn.each do |unit|
-    #counter = 0
     if unit == "X" || unit == "O"
-      #puts "spot taken"
       move_count.push(1)
-    #else
-      #puts "spot free"
-      #counter += 1
     end
   end
+
   move_count.length.to_i
 end
 
