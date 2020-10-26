8dc0e7b6753830fc9695ffb59deab428a8d969a1
Greg Shivcharran <greg.shivcharran@gmail.com>
2020-10-26 16:57:39 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  count = 0
+  board.each do |x|
+    if x == "X" or x == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 1
+    return "O"
+  elsif turn_count(board) % 2 == 0
+    return "X"
+  end
+end
